615ad3144e0f4671226740abd463d5967444db8a
Thomas Li <thomas.li@fedex.com>
2024-06-14 14:28:46 -0500

    Nav Bar work

--- GUI/src/app/app.routes.ts
+++ GUI/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { StartComponent } from './start/start.component';
+import { ResultsComponent } from './results/results.component';
 
-export const routes: Routes = [];
+export const routes: Routes =
+[ {path: '', component: HomepageComponent},
+  {path: 'start', component: StartComponent},
+  {path: 'results', component: ResultsComponent}
+ ];
--- GUI/src/app/navbar/navbar.component.ts
+++ GUI/src/app/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
 }
+
